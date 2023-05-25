9aff9f2df1d3764d89d023003bcca3443b04448f
Bryan Schuetz <bschuetz@gmail.com>
2023-05-25 14:09:27 -0400

    More Better

--- src/py-crap/bot.py
+++
@@ -1,7 +0,0 @@
-import slack
-
-# O-auth token for slack bot
-SLACK_TOKEN="xoxb-2190897728-5030141376935-fFwIcIn08qafOSzrzwHSmyHb"
-
-client = slack.WebClient(token=SLACK_TOKEN)
-client.chat_postMessage(channel='#d-ai',text='Hello')
